3aa7a543130443ea42c34d2978be3bb225df3f2f
rajeshraoji93 <134900907+rajeshraoji93@users.noreply.github.com>
2024-02-04 13:42:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,5 @@ streamlit.header("Fruityvice Fruit Advice!")
 #api request response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
